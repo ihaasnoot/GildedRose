dab9b826239068e2a03b3b9156d834a98ba0b74b
Irene Haasnoot <i.haasnoot@gmail.com>
2020-09-21 17:54:26 +0200

    #1 refactor existing code for better readability - remove unnecessary nested if statements - prevent NPE and typos for name comparison - add javadoc and comments

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,59 +1,69 @@
 package com.gildedrose;
 
+/**
+ * Class for the inventory of the Gilded Rose
+ */
 class GildedRose {
+
+    private static final String BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
+    private static final String BRIE = "Aged Brie";
+    private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+
     Item[] items;
 
     public GildedRose(Item[] items) {
         this.items = items;
     }
 
+    /**
+     * Updates the quality of the items in inventory depending on the item
+     */
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
+        for (Item item : items) {
+            //decrease quality of ordinary items
+            if (!BRIE.equals(item.name)
+                && !BACKSTAGE.equals(item.name)
+                && item.quality > 0
+                && !SULFURAS.equals(item.name)) {
+                    item.quality--;
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+                //increase quality of "better with age"-items
+                if (item.quality < 50) {
+                    item.quality++;
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
+                    //extra increase of quality of backstage passes depending on sellIn date
+                    if (BACKSTAGE.equals(item.name)) {
+                        if (item.sellIn < 11 && item.quality < 50) {
+                            item.quality++;
                         }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
+                        if (item.sellIn < 6 && item.quality < 50) {
+                            item.quality++;
                         }
                     }
                 }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
+            //decrease sellIn date for all items except Sulfuras
+            if (!SULFURAS.equals(item.name)) {
+                item.sellIn--;
             }
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
+            //extra update of quality when sellIn date has past
+            if (item.sellIn < 0) {
+                //extra decrease of quality of ordinary items
+                if (!BRIE.equals(item.name)) {
+                    if (!BACKSTAGE.equals(item.name)) {
+                        if(item.quality > 0 && !SULFURAS.equals(item.name)) {
+                            item.quality--;
                         }
+                      //set quality of backstage passes at zero
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality;
+                        item.quality = 0;
                     }
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
+                    //extra increase of quality of brie
+                    if (item.quality < 50) {
+                        item.quality++;
                     }
                 }
             }
